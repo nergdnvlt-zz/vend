267c771e7618ca605e66264ae1208de2810498bd
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-06 10:53:07 -0600

    Finished getting show page up and name on it Added TDD for price.

--- spec/features/user_can_visit_snack_show_page_spec.rb
+++ spec/features/user_can_visit_snack_show_page_spec.rb
@@ -9,6 +9,15 @@ describe 'snack show page' do
     visit snack_path(snack)
 
     expect(page).to have_content(snack.name)
+  end
+
+  scenario 'they see the snack price' do
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    snack = dons.snacks.create!(name: 'Snickers', price: 2.0)
+
+    visit snack_path(snack)
+
     expect(page).to have_content(snack.price)
   end
 end
